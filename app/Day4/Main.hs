78967666a2f8fc2d4ebd3fbb898f627ced6a5694
James King <james@agentultra.com>
2018-12-05 21:28:54 -0500

    Modify src/Day4.hs
    Fixed it.
    
    Cleaned up formatting.

--- app/Day4/Main.hs
+++ app/Day4/Main.hs
@@ -9,14 +9,14 @@ import           Protolude
 import Day4
 
 main :: IO ()
-main = do 
-    let fileName = "./input/Day04.txt"
-    input <- T.readFile fileName
-    let m = M.map sleepIntervals . partitionEvents <$> mapM parseLine (T.lines input)
-    case m of
-        Right m -> do
-            let gid@(GuardId n) = sleepyHead m                                                
-            print $ time (maximumBy (compare `on` duration) $ m ! gid) * n -- this number though is the same as guessed last time
-            print n 
-            print $ time (maximumBy (compare `on` duration) $ m ! gid) * n
-        Left err -> print err
+main = do
+  let fileName = "./input/Day04.txt"
+  input <- T.readFile fileName
+  let partitionedEvents = partitionEvents <$> mapM parseLine (T.lines input)
+  let m = M.map sleepIntervals <$> partitionedEvents
+  case m of
+    Right m -> do
+      let gid@(GuardId n) = sleepyHead m
+      let foo = maximumBy (compare `on` snd) $ M.toList $ asleepMinutes $ m ! gid
+      print (((fst foo)) * n)
+    Left err -> print err
--- src/Day4.hs
+++ src/Day4.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Day4 where
 
+import Control.Lens
 import Data.Time hiding (Day)
-import Data.Map as M
+import qualified Data.Map as M
 import Data.List as L
 import Prelude (read)
 import Protolude hiding ((<|>), many)
@@ -16,26 +19,27 @@ type Minute = Int
 newtype GuardId = GuardId Int
     deriving (Eq,Ord,Show)
 
-data Date = Date Year Month Day Hour Minute
-    deriving (Eq,Ord,Show)
+data Date =
+  Date
+  { _year :: Year
+  , _month :: Month
+  , _day :: Day
+  , _hour :: Hour
+  , _minute :: Minute
+  }
+  deriving (Eq,Ord,Show)
+
+makeLenses ''Date
 
 data Event = ShiftStart GuardId | Asleep | WakeUp
     deriving (Eq,Ord,Show)
 
 data Row =
   Row
-  { date :: UTCTime
+  { date :: Date
   , event :: Event
   }
   deriving (Eq,Ord,Show)
-
-    -- , "[1518-10-03 00:35] wakes up"
-    -- , "[1518-04-30 00:20] wakes up"
-    -- , "[1518-10-15 23:59] Guard #1699 begins shift"
-    -- , "[1518-06-07 00:39] wakes up"
-    -- , "[1518-09-25 00:10] falls asleep"
-    -- , "[1518-09-27 00:30] falls asleep"
-    -- , "[1518-11-03 00:36] falls asleep"
 
 parseEvent :: Parsec Text () Event
 parseEvent =
@@ -46,12 +50,6 @@ parseEvent =
 parseInt :: Maybe Int -> Parsec Text () Int
 parseInt = fmap read . maybe (many digit) (`count` digit)
 
-mkDate :: Year -> Month -> Day -> Hour -> Minute -> UTCTime
-mkDate yy mm dd hh min =
-  UTCTime (fromGregorian (toInteger yy) mm dd)
-  (secondsToDiffTime $ toInteger $ (60 * hh + min) * 60)
-
-
 lineParser :: Parsec Text () Row
 lineParser = do
     char '['
@@ -61,7 +59,7 @@ lineParser = do
     hh <- parseInt (Just 2) <* char ':'
     min <- parseInt (Just 2)
     char ']' >> char ' '
-    let d = mkDate yy mm dd hh min
+    let d = Date yy mm dd hh min
     Row d <$> parseEvent
 
 parseLine :: Text -> Either ParseError Row
@@ -80,15 +78,16 @@ tagEvent xs = catMaybes $ evalState (mapM tag $ L.sortOn date xs) (Nothing :: Ma
 
 data Interval =
   Interval
-  { duration :: NominalDiffTime
-  , time :: Minute
+  { duration :: Minute
+  , start :: Minute
+  , end :: Minute
   }
   deriving (Eq, Show)
 
 sleepIntervals :: [Row] -> [Interval]
-sleepIntervals xs = catMaybes $ evalState (mapM tag xs) (Nothing :: Maybe UTCTime)
+sleepIntervals xs = catMaybes $ evalState (mapM tag xs) (Nothing :: Maybe Date)
   where
-    tag :: Row -> State (Maybe UTCTime) (Maybe Interval)
+    tag :: Row -> State (Maybe Date) (Maybe Interval)
     tag (Row d Asleep) = do
       s <- get
       maybe (put $ Just d) (const $ pure ()) s
@@ -99,8 +98,15 @@ sleepIntervals xs = catMaybes $ evalState (mapM tag xs) (Nothing :: Maybe UTCTim
         Nothing -> pure Nothing
         Just d' -> do
           put Nothing
-          pure $ Just $ Interval ((d `diffUTCTime` d') - 60) (((floor (toRational $ utctDayTime d) `div` 60) `mod` 60) - 1)
-    tag _doh = pure Nothing
+          pure $ Just $ Interval ((d^.minute - 1) - d'^.minute) (d'^.minute) (d^.minute)
+    tag _ = pure Nothing
 
 sleepyHead :: Map GuardId [Interval] -> GuardId
 sleepyHead = fst . L.maximumBy (compare `on` snd) . M.toList . M.map (L.sum . L.map duration)
+
+asleepMinutes :: [Interval] -> Map Minute Int
+asleepMinutes = foldl' countMins (M.fromList [(x, 0) | x <- [0..59]])
+  where
+    countMins :: Map Minute Int -> Interval -> Map Minute Int
+    countMins acc (Interval _ start end) =
+      M.mapWithKey (\m c -> if start <= m && m <= end then c + 1 else c) acc
